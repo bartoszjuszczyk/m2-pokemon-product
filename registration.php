70b23c08e8de519c27f15caccb9a953e7fed80f8
Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
2024-10-25 23:15:41 +0200

    Initial module

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * File: registration.php
+ *
+ * @author Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
+ * @copyright Copyright (c) 2024.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Juszczyk_PokemonProduct', __DIR__);
